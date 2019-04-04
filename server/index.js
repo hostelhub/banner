3ab0007c24babd5ebc038726bf6643d3d5cfe5e8
Leslie Rodriguez Cabrera <leslie.roca56@gmail.com>
2019-04-03 22:02:23 -0700

    Handel GET by name and GET all request

--- server/index.js
+++ server/index.js
@@ -1,21 +1,42 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const cors = require('cors');
-const Hostels = require('../database/index.js');
-const mongoose = require('../database/index.js');
-
+//const cors = require('cors');
+const hostel = require('../database/hostel.js');
+// const mongoose = require('../database/index.js');
 
 const app = express();
-const PORT = 3000;
+const port = 8000;
 
-
+app.use(bodyParser.urlencoded());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(express.static(__dirname + '/../client/dist'));
 
+
 app.get('/api/hostels', function(req, res) {
-  Hostels.find(function(err, hostels) {
-    res.send(hostels);
+  console.log("Request made to /api/hostels");
+  hostel.findAll(function(err, hostels) {
+    if (err) {
+      res.send(err);
+    } else {
+      res.send(hostels);
+    }
   })
 });
+
+app.get('/api/hostel/:name', function(req, res) {
+  console.log("Request made to /api/hostel/:name");
+    hostel.findByName(req.params.name, function(err, hostel) {
+      if (err) {
+        res.send(err);
+      } else {
+        res.send(hostel);
+      }
+    })
+})
+
+app.listen(port, () => console.log(`Listening on port ${port}!`))
+
+
+
+
