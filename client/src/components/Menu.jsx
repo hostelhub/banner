01e7ba882593e822d01f66d9c6bdc6da43c81fa3
Leslie Rodriguez Cabrera <leslie.roca56@gmail.com>
2019-04-05 03:40:27 -0700

    Creat the four buttons for Menu component

--- client/src/components/Menu.jsx
+++ client/src/components/Menu.jsx
@@ -11,6 +11,25 @@ class Menu extends React.Component {
         <h1>
           HELLO FROM MENU
         </h1>
+        <i className='fas fa-user'></i>
+        <button className="sign-in-btn"
+        // onClick={this.handleUserOnClick.bind(this)}
+        ><i className="fa fa-user"></i>User</button>
+
+        <i className='fas fa-language'></i>
+        <button className="language-btn"
+         // onClick={this.handleLanguageOnClick.bind(this)}
+        ><i className="fa fa-language"></i>EN</button>
+
+        <i className='fas fa-search'></i>
+        <button className="search-btn"
+        // onClick={this.handleSearchOnClick.bind(this)}
+        ><i className="fa fa-search"></i>Search</button>
+
+        <i className='fas fa-sidebar'></i>
+        <button className="sidebar-btn"
+        // onClick={this.handleSidebarOnClick.bind(this)}
+        ><i className="fa fa-sidebar"></i>=</button>
       </div>
     );
   }
